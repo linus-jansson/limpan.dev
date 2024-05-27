ee86f84fdcbcbf893b72e49fca5e8dcec751e007
Linus Jansson <contact@limpan.dev>
2024-05-27 20:38:21 +0200

    refactor: polish up cookiebanner

--- src/app/cnct/cookieBanner.tsx
+++ src/app/cnct/cookieBanner.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { FormEvent, useCallback, useState } from "react"
 import { acceptCookies } from "../server-actions"
+import Link from "next/link"
 
 export const CookieBanner = ({cookiesAccepted}: {cookiesAccepted: boolean}) => {
     const [show, setShow] = useState(!cookiesAccepted)
@@ -17,14 +18,18 @@ export const CookieBanner = ({cookiesAccepted}: {cookiesAccepted: boolean}) => {
         return null;
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 py-8 border-t border-border">
-            <div className="flex items-center justify-between container mx-auto">
-                <p className="text-sm text-muted-foreground flex gap-4 items-center">
+        <div className="fixed bottom-0 left-0 right-0 p-8 border-t bg-base-100 border-border">
+            <div className="flex items-center justify-between container mx-auto flex flex-wrap space-y-4 md:space-y-0">
+                <div className="flex gap-4 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    This page uses cookies to make the captcha work. <span className="font-bold">To continue, please accept the use of cookies.</span>
-                </p>
-                <form onSubmit={handleCookieFormSubmit}>
+                    <p className="text-sm text-muted-foreground">
+                        This page uses cookies to make the captcha work. 
+                    </p>
+                    <span className="font-bold">To continue, please accept the use of cookies.</span>
+                </div>
+                <form onSubmit={handleCookieFormSubmit} className="flex gap-4">
                     <button className="btn">Accept</button>
+                    <Link href="/" className="btn">Do not accept</Link>
                 </form>
             </div>
         </div>
