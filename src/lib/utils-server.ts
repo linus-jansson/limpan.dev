dd7d05b6e31c8bf5476b1675cc7f5548fa59256b
Linus J <30862662+linus-jansson@users.noreply.github.com>
2024-06-12 15:22:35 +0200

    refactor: split up verify turnstile to util function

--- src/lib/utils-server.ts
+++ src/lib/utils-server.ts
@@ -2,6 +2,11 @@ import "server-only";
 import fs from "fs";
 
 const PATH_TO_PUBLIC_KEY = 'src/app/pgp/limpan.pgp-key.asc.old'
+const CF_SECRET = process.env.CF_SECRET!;
+
+import { CF_VERIFY_URL, } from "@/lib/constants";
+import { TurnstileServerValidationResponse } from "@marsidev/react-turnstile";
+
 
 export const getPgpPublicKey = async (): Promise<string> => {
     return new Promise((resolve, reject
@@ -15,4 +20,31 @@ export const getPgpPublicKey = async (): Promise<string> => {
         })
     
     })
+}
+
+export const verifyCloudflareTurnstile = async (cf_verification_token : string) => {
+    if (!cf_verification_token) {
+        return new Response('No token provided', {
+            status: 400,
+            headers: {
+                'content-type': 'text/plain'
+            }
+        })
+    }
+
+    const res = await fetch(CF_VERIFY_URL, {
+        method: 'POST',
+        body: `secret=${encodeURIComponent(CF_SECRET)}&response=${encodeURIComponent(cf_verification_token)}`,
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    })
+
+    try {
+        const data = (await res.json()) as TurnstileServerValidationResponse
+        return data.success;
+    } catch (error) {
+        console.error("Captchea verification failed with error:", error);
+        return false;        
+    }
 }
