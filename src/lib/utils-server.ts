67b49b601ca865299e3e70bd0235f9a6ad0061c5
Linus Jansson <linus.jansson2@uppsala.se>
2024-06-23 23:39:42 +0200

    fix: remove fs causing runtime error on serverless?

--- src/lib/utils-server.ts
+++ src/lib/utils-server.ts
@@ -1,26 +1,10 @@
 import "server-only";
-import fs from "fs";
 
-const PATH_TO_PUBLIC_KEY = 'src/app/pgp/limpan.pgp-key.asc.old'
 const CF_SECRET = process.env.CF_SECRET!;
 
 import { CF_VERIFY_URL, } from "@/lib/constants";
 import { TurnstileServerValidationResponse } from "@marsidev/react-turnstile";
 
-
-export const getPgpPublicKey = async (): Promise<string> => {
-    return new Promise((resolve, reject
-    ) => {
-        fs.readFile(PATH_TO_PUBLIC_KEY, 'utf8', (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    
-    })
-}
 
 export const verifyCloudflareTurnstile = async (cf_verification_token : string) => {
     if (!cf_verification_token) {
