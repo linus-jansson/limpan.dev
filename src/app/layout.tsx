11e5c1f18a64e950e7369d3bfd8985fa9bdc2825
Linus Jansson <linus.jansson2@uppsala.se>
2025-03-29 19:33:12 +0100

    feat: adding custom viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,8 @@ export const metadata: Metadata = {
       'theme-color': '#18181B',
       'apple-mobile-web-app-capable': 'true',
       'apple-mobile-web-app-status-bar-style': 'black-translucent',
-  }
+  },
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover"
 };
 
 export default function RootLayout({
